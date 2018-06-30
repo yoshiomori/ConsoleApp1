fb313c48342ec048aa6992058ac225aa0c2a114f
Yoshio Mori <yoshio.mori@hotmail.com>
2018-06-30 20:29:48 +0000

    Código

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var A = Matrix<double>.Build.DenseOfArray(new double[,] {
+                { 3, 2, -1 },
+                { 2, -2, 4 },
+                { -1, 0.5, -1 }
+            });
+            var b = Vector<double>.Build.Dense(new double[] { 1, -2, 0 });
+            var x = A.Solve(b);
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+    }
+}
