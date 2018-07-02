b56febc0660f1afbf4ffdd8d51428b063771a011
Yoshio Mori <yoshio.mori@hotmail.com>
2018-07-01 23:05:39 -0300

    Leitura das linhas e resolução caso double

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,14 +11,36 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            var A = Matrix<double>.Build.DenseOfArray(new double[,] {
-                { 3, 2, -1 },
-                { 2, -2, 4 },
-                { -1, 0.5, -1 }
-            });
-            var b = Vector<double>.Build.Dense(new double[] { 1, -2, 0 });
-            var x = A.Solve(b);
-            Console.WriteLine(x);
+            var line = Console.ReadLine();
+            var response = new List<Vector<double>>();
+            while (line.Length > 0)
+            {
+                var words = line.Split(' ');
+                var n = int.Parse(words[0]);
+                var rows = n * (n - 1) / 2;
+                var A = Matrix<double>.Build.Dense(rows, n);
+                for (int i = 0, j = 0, k = 1; i < rows; i++)
+                {
+                    A[i, j] = 1;
+                    A[i, k] = 1;
+                    k++;
+                    if (k >= n)
+                    {
+                        j++;
+                        k = j + 1;
+                    }
+                }
+                var b = Vector<double>.Build.Dense(rows, i => double.Parse(words[i + 1]));
+                response.Add(A.Solve(b));
+                line = Console.ReadLine();
+            }
+            foreach (var x in response)
+                Console.WriteLine(
+                    x
+                    .ToArray()
+                    .Select(v=>v.ToString())
+                    .Aggregate((s1, s2) => string.Format("{0} {1}", s1, s2))
+                );
             Console.ReadLine();
         }
     }
